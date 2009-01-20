c212ac02a33ee8c3569f7bde602b70d712f0d5e2
Adam Winter <adam.e.winter@gmail.com>
2009-01-20 11:04:01 -0500

    added admin flag

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090120150513) do
+ActiveRecord::Schema.define(:version => 20090120155339) do
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20090120150513) do
     t.string   "crypted_password"
     t.string   "password_salt"
     t.string   "persistence_token"
+    t.boolean  "admin"
   end
 
 end
