084b096e404654404a5a9b6f249c1fcea900dc71
Adam Winter <adam.e.winter@gmail.com>
2009-01-21 08:50:31 -0500

    trying to resolve issue with many-to-many relationship

--- db/migrate/20090120171958_add_friends_to_users.rb
+++ db/migrate/20090120171958_add_friends_to_users.rb
@@ -1,12 +1,12 @@
 class AddFriendsToUsers < ActiveRecord::Migration
   def self.up
-    create_table :friendships do |t|
+    create_table :friendships, :id => false do |t|
       t.integer :user_id
       t.integer :friend_id
     end
   end
 
   def self.down
-    drop_table :friendship
+    drop_table :friendships
   end
 end
